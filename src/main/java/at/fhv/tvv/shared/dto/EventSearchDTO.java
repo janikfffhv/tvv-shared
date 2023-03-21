67d6aced61db0c0cfc872731eb091b8f2db292df
janikfffhv <janik.fuchsberger@students.fhv.at>
2023-03-21 15:19:53 +0100

    Initial commit

---
+++ src/main/java/at/fhv/tvv/shared/dto/EventSearchDTO.java
@@ -0,0 +1,51 @@
+package at.fhv.tvv.shared.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class EventSearchDTO implements Serializable {
+    private final int eventId;
+
+    private final String name;
+
+    private final String veranstaltungsserie;
+
+    private final Date datum;
+
+    private final String ort;
+
+    private final int plaetze;
+
+    public EventSearchDTO (int eventId, String name, String veranstaltungsserie, Date datum, String ort, int plaetze) {
+        this.eventId = eventId;
+        this.name = name;
+        this.veranstaltungsserie = veranstaltungsserie;
+        this.datum = datum;
+        this.ort = ort;
+        this.plaetze = plaetze;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVeranstaltungsserie() {
+        return veranstaltungsserie;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public int getPlaetze() {
+        return plaetze;
+    }
+}
---
+++ src/main/java/at/fhv/tvv/shared/rmi/EventSearch.java
@@ -0,0 +1,13 @@
+package at.fhv.tvv.shared.rmi;
+import at.fhv.tvv.shared.dto.EventSearchDTO;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
+
+
+public interface EventSearch extends Remote {
+    List<EventSearchDTO> searchByString(String searchString) throws RemoteException;
+    List<EventSearchDTO> searchByDate(Date searchDate) throws RemoteException;
+}
