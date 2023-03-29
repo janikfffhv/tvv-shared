90168a206f54bb48b3550b218ef8cd4e9c81293f
janikfffhv <janik.fuchsberger@students.fhv.at>
2023-03-29 10:37:38 +0200

    Added Search By Category

--- src/main/java/at/fhv/tvv/shared/rmi/EventSearch.java
+++ src/main/java/at/fhv/tvv/shared/rmi/EventSearch.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface EventSearch extends Remote {
     List<EventSearchDTO> searchByString(String searchString) throws RemoteException;
     List<EventSearchDTO> searchByDate(int searchDate1, int searchDate2) throws RemoteException;
+    List<EventSearchDTO> searchByCategory(String searchString) throws RemoteException;
 }
