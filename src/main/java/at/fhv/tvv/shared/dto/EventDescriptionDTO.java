c26539a7525d504750b55582d512b4b53cebdc76
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-04-04 13:15:02 +0200

    Added Event Description

--- src/main/java/at/fhv/tvv/shared/dto/EventDescriptionDTO.java
+++ src/main/java/at/fhv/tvv/shared/dto/EventDescriptionDTO.java
@@ -9,20 +9,33 @@ public class EventDescriptionDTO implements Serializable {
     private final String name;
 
     private final String veranstaltungsserie;
+    private final String veranstalter;
+    private final String kategorie;
 
     private final int datum;
-
+    private final String gebaeude;
+    private final String strasse;
+    private final String hausnummer;
+    private final int plz;
     private final String ort;
+    private final String raum;
 
     private final String beschreibung;
     private final int plaetze;
 
-    public EventDescriptionDTO(int eventId, String name, String veranstaltungsserie, int datum, String ort, String beschreibung, int plaetze) {
+    public EventDescriptionDTO(int eventId, String name, String veranstaltungsserie, String veranstalter, String kategorie, int datum, String gebaeude, String strasse, String hausnummer, int plz, String ort, String raum, String beschreibung, int plaetze) {
         this.eventId = eventId;
         this.name = name;
         this.veranstaltungsserie = veranstaltungsserie;
+        this.veranstalter = veranstalter;
+        this.kategorie = kategorie;
         this.datum = datum;
+        this.gebaeude = gebaeude;
+        this.strasse = strasse;
+        this.hausnummer = hausnummer;
+        this.plz = plz;
         this.ort = ort;
+        this.raum = raum;
         this.beschreibung = beschreibung;
         this.plaetze = plaetze;
     }
@@ -55,17 +68,45 @@ public class EventDescriptionDTO implements Serializable {
         return plaetze;
     }
 
+    public String getVeranstalter() {
+        return veranstalter;
+    }
+
+    public String getKategorie() {
+        return kategorie;
+    }
+
+    public String getGebaeude() {
+        return gebaeude;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public String getHausnummer() {
+        return hausnummer;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public String getRaum() {
+        return raum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         EventDescriptionDTO that = (EventDescriptionDTO) o;
-        return eventId == that.eventId && datum == that.datum && plaetze == that.plaetze && Objects.equals(name, that.name) && Objects.equals(veranstaltungsserie, that.veranstaltungsserie) && Objects.equals(ort, that.ort) && Objects.equals(beschreibung, that.beschreibung);
+        return eventId == that.eventId && datum == that.datum && plz == that.plz && plaetze == that.plaetze && Objects.equals(name, that.name) && Objects.equals(veranstaltungsserie, that.veranstaltungsserie) && Objects.equals(veranstalter, that.veranstalter) && Objects.equals(kategorie, that.kategorie) && Objects.equals(gebaeude, that.gebaeude) && Objects.equals(strasse, that.strasse) && Objects.equals(hausnummer, that.hausnummer) && Objects.equals(ort, that.ort) && Objects.equals(raum, that.raum) && Objects.equals(beschreibung, that.beschreibung);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(eventId, name, veranstaltungsserie, datum, ort, beschreibung, plaetze);
+        return Objects.hash(eventId, name, veranstaltungsserie, veranstalter, kategorie, datum, gebaeude, strasse, hausnummer, plz, ort, raum, beschreibung, plaetze);
     }
 }
 
