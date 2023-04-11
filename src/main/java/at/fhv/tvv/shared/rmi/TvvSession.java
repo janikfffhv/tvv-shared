9e3cfb3d28b98bbd30134ee9655b0b6b2da1f1b1
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-04-11 17:05:27 +0200

    Kunde und Zahlungsmethode hinzugefügt

--- src/main/java/at/fhv/tvv/shared/rmi/TvvSession.java
+++ src/main/java/at/fhv/tvv/shared/rmi/TvvSession.java
@@ -5,10 +5,16 @@ import at.fhv.tvv.shared.dto.WarenkorbZeileDTO;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.UUID;
 
 public interface TvvSession extends Remote {
     List<WarenkorbZeileDTO> getWarenkorb() throws RemoteException;
+    UUID getKunde() throws RemoteException;
+    String getZahlungsMethode() throws RemoteException;
     void hinzufuegen(WarenkorbZeileDTO warenkorbZeile) throws RemoteException;
     void loeschen (WarenkorbZeileDTO warenkorbZeile) throws RemoteException;
     void leeren() throws RemoteException;
+
+    void hinzufuegenKunde(UUID kunde) throws RemoteException;
+    void hinzufuegenZahlungsMethode(String zahlungsmethode) throws RemoteException;
 }
