de5b90b88b18ca83b3381cce1b5637127ac63d06
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-04-12 02:41:15 +0200

    Bugfix 4

---
+++ src/main/java/at/fhv/tvv/shared/rmi/CustomerUUID.java
@@ -0,0 +1,11 @@
+package at.fhv.tvv.shared.rmi;
+
+import at.fhv.tvv.shared.dto.CustomerInfoDTO;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+public interface CustomerUUID extends Remote {
+    CustomerInfoDTO searchById(UUID id) throws RemoteException;
+}
