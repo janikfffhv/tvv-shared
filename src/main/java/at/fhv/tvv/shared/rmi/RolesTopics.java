8478dcf687e795b4b32b9f73693409e86d2bbc66
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-04-26 10:57:52 +0200

    Added Roles and Topics 2.0

---
+++ src/main/java/at/fhv/tvv/shared/rmi/RolesTopics.java
@@ -0,0 +1,11 @@
+package at.fhv.tvv.shared.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface RolesTopics extends Remote {
+    List<String> getRoles(String userId) throws RemoteException;
+    List<String> getTopics(String userId) throws RemoteException;
+    void setTopics(List<String> topics) throws RemoteException;
+}
