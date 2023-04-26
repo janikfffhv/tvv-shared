7a31106813a303ce534902ec80b6a83b9604a311
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-04-26 11:07:22 +0200

    added Username to add Topics

--- src/main/java/at/fhv/tvv/shared/rmi/RolesTopics.java
+++ src/main/java/at/fhv/tvv/shared/rmi/RolesTopics.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface RolesTopics extends Remote {
     List<String> getRoles(String userId) throws RemoteException;
     List<String> getTopics(String userId) throws RemoteException;
-    void setTopics(List<String> topics) throws RemoteException;
+    void setTopics(List<String> topics, String userId) throws RemoteException;
 }
