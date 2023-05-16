c930b5349b55e382415732cb37b42796f142b1cd
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-05-16 15:07:28 +0200

    bugfix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module tvv.shared {
+    requires java.rmi;
+    requires javax.ejb.api;
     exports at.fhv.tvv.shared.ejb;
     exports at.fhv.tvv.shared.dto;
     exports at.fhv.tvv.shared.rmi;
+
 }
