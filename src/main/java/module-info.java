3eb308742238eb5b132f8fbd24eaf0b6c1d82c21
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-05-16 16:25:13 +0200

    For shared backend

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module tvv.shared {
-    requires java.rmi;
-    requires javax.ejb.api;
-    exports at.fhv.tvv.shared.ejb;
-    exports at.fhv.tvv.shared.dto;
-    exports at.fhv.tvv.shared.rmi;
-
-}
