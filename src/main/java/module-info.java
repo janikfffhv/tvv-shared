20e448b7554f9e8185c29404f367a2b854bae04a
janikfffhv <80453751+janikfffhv@users.noreply.github.com>
2023-05-17 11:27:02 +0200

    For shared backend_Boolean

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module tvv.shared {
     requires java.rmi;
-    requires javax.ejb.api;
+    requires beta.jboss.ejb.api_3_2;
     exports at.fhv.tvv.shared.ejb;
     exports at.fhv.tvv.shared.dto;
     exports at.fhv.tvv.shared.rmi;
